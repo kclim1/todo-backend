81e363e2450b495983b5d6c0c2b3f0a37d845171
kclim1 <kclim0528@gmail.com>
2025-05-08 23:32:04 +0800

    setup google auth to return profile object for inspection

---
+++ src/auth/googleAuth.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+const googleStrategy = passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.CALLBACK_URL
+  },
+  async (accessToken, refreshToken, profile, done) => {
+    console.log("Google Profile:", profile);
+    done(null, profile);
+  }
+));
+
+module.exports = googleStrategy
