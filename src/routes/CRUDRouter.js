34749ac9e6d217b54e3e531532a564dda1c6b721
kclim1 <kclim0528@gmail.com>
2025-05-08 21:51:06 +0800

    setup test route in router file

--- src/routes/CRUDRouter.js
+++ src/routes/CRUDRouter.js
@@ -2,7 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
+    console.log('root route hit')
     res.send('Hello from router');
   });
+
+
 
 module.exports = router
