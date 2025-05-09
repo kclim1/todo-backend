26385d7a81cd845420ff9482c06aecbf232db616
kclim1 <kclim0528@gmail.com>
2025-05-09 21:02:34 +0800

    refactored router to follow MVC principles

--- src/routes/CRUDRouter.js
+++ src/routes/CRUDRouter.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const CRUDController = require('../controllers/CRUDController')
 
 
-
-
+router.post('/todos',CRUDController.createTodo)
 
 
 module.exports = router
