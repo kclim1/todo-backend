1d7d6a5238d7649ef795c2c859c45a6b44a1baed
kclim1 <kclim0528@gmail.com>
2025-05-09 16:43:53 +0800

    refactored googleAuth logic to check for existing user.

--- src/auth/googleAuth.js
+++ src/auth/googleAuth.js
@@ -12,21 +12,32 @@ const googleStrategy = passport.use(
       callbackURL: process.env.CALLBACK_URL
     },
     async (accessToken, refreshToken, profile, done) => {
+        
       try {
-        const user = {
+        let googleUser = await User.findOne({ googleId: profile.id });
+        if(!googleUser){
+            googleUser = await User.create({
+                googleId:profile.id,
+                userName:profile.displayName,
+                email:profile.emails[0].value
+            })
+        }
+        
+        let user = {
           googleId: profile.id,
           email: profile.emails[0].value,
-          name: profile.name.givenName
+          userName: profile.displayName
         };
 
         const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "1h" });
 
         user.token = token;
-
+        
         done(null, user);
+
       } catch (error) {
         console.error("Error logging in with Google:", error);
-        done(error, null); // Ensure the error is passed to Passport
+        done(error, null); 
       }
     }
   )
