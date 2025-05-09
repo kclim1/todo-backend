2d020699f340495fc314b45d9ea8e9dac1c31b75
kclim1 <kclim0528@gmail.com>
2025-05-09 16:45:16 +0800

    removed test function inside CRUDRouter

--- src/routes/CRUDRouter.js
+++ src/routes/CRUDRouter.js
@@ -1,10 +1,6 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/dashboard', (req, res) => {
-    console.log('root route hit')
-    res.json({sample:"todo"});
-  });
 
 
 
