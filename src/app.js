6e70915df249bc29877f9d09644fac894d8af5cb
kclim1 <kclim0528@gmail.com>
2025-05-09 16:43:35 +0800

    reorganized app.js structure and mounted a new router

--- src/app.js
+++ src/app.js
@@ -2,10 +2,11 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-const PORT = process.env.PORT || 3000;
-const CRUDRouter = require('./routes/CRUDRouter');
+const PORT = process.env.PORT 
 const connectToMongoDB = require('./config/connectToMongoDB');
+const CRUDRouter = require('./routes/CRUDRouter');
 const authRouter = require('./routes/authRouter')
+const pageRoutes = require('./routes/pageRoutes')
 const passport = require('passport');
 require('./auth/googleAuth');
 
@@ -20,6 +21,8 @@ app.use(express.json());
 
 //Can add github auth or local auth in the future
 app.use("/auth", authRouter);
+
+app.use('/',pageRoutes)
 
 // Mount the CRUD Router (API Routes)
 app.use('/api/v1', CRUDRouter);
