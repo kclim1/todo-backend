dd3d615491c5ee7493d039116c2f4ded39cf6231
kclim1 <kclim0528@gmail.com>
2025-05-10 17:11:40 +0800

    fixed typo in name

--- src/routes/pageRouter.js
+++ src/routes/pageRouter.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-//can be used for pages like contacts , faq , about , etc 
+//can be used for pages like contacts , faq , about , etc. serves as placeholder for now
 
 router.get('/dashboard', (req, res) => {
     console.log('root route hit')
